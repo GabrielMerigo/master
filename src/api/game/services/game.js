9356c511aa7c2e67efe08640badecb3680c19647
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2022-04-20 17:50:22 -0300

    more accs

--- src/api/game/services/game.js
+++ src/api/game/services/game.js
@@ -1,14 +1,14 @@
 'use strict';
 
-/**
- * game service.
- */
-
 const { createCoreService } = require('@strapi/strapi').factories;
+const axios = require('axios');
+
+function getGameInfo
 
 module.exports = createCoreService('api::game.game', ({ strapi }) => ({
   populate: async (params) => {
-    const cat = await strapi.service('api::category.category').find()
-    console.log(cat)
+    const gogAPIUrl = `https://catalog.gog.com/v1/catalog?limit=48&price=between%3A80%2C380&order=desc%3Atrending&productType=in%3Agame%2Cpack&page=1&countryCode=BR&locale=en-US&currencyCode=BRL`;
+    const { data: { products } } = await axios.get(gogAPIUrl);
+    console.log(products[0])
   }
 }));
