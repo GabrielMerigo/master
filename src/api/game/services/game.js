871e35dbb908cdd4d8087202d38d51fa6a7cc699
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2022-04-22 03:23:47 -0300

    many to many done!

--- src/api/game/services/game.js
+++ src/api/game/services/game.js
@@ -45,12 +45,35 @@ async function create(name, entityName){
   }
 }
 
+async function createManyToManyData(products) {
+  const developers = {};
+  const publishers = {};
+  const categories = {};
+  const platforms = {};
+
+  products.forEach((product) => {
+    const { developers: developer , publishers: publisher, genres, operatingSystems } = product;
+
+    genres && genres.forEach(({name}) => { categories[name] = true; });
+    operatingSystems && operatingSystems.forEach((item) => { platforms[item] = true });
+
+    developers[developer] = true;
+    publishers[publisher] = true;
+  });
+
+  return Promise.all([
+    ...Object.keys(developers).map((name) => create(name, "developer")),
+    ...Object.keys(publishers).map((name) => create(name, "publisher")),
+    ...Object.keys(categories).map((name) => create(name, "category")),
+    ...Object.keys(platforms).map((name) => create(name, "platform")),
+  ]);
+}
+
 module.exports = createCoreService('api::game.game', ({ strapi }) => ({
   populate: async (params) => {
     const gogAPIUrl = `https://catalog.gog.com/v1/catalog?limit=48&price=between%3A80%2C380&order=desc%3Atrending&productType=in%3Agame%2Cpack&page=1&countryCode=BR&locale=en-US&currencyCode=BRL`;
     const { data: { products } } = await axios.get(gogAPIUrl);
 
-    console.log(await create(products[0].developers[0], 'developer'))
-
+    // console.log(await createManyToManyData(products));
   }
 }));
